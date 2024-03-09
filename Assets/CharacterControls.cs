3af478ba431da53b9c747bf6820ce84f8b0bd61a
bob <totesanders@gmail.com>
2024-03-09 07:35:50 -0800

    Added basic movement for a 2d type of game
    wasd

--- Assets/CharacterControls.cs
+++ Assets/CharacterControls.cs
@@ -1,22 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CharacterControls : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public float Speed;
     // Start is called before the first frame update
+    private string userInput;
     void Start()
     {
-
+        Debug.Log("Hi");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        userInput = Input.inputString;
+        switch (userInput)
         {
-            myRigidbody.velocity = Vector2.up * 10;
+            case "w":
+                Debug.Log("Moving forward");
+                myRigidbody.velocity = Vector2.up * Speed;
+                break;
+            case "a":
+                Debug.Log("Moving left");
+                myRigidbody.velocity = Vector2.left * Speed;
+                break;
+            case "s":
+                Debug.Log("Moving down");
+                myRigidbody.velocity = Vector2.down * Speed;
+                break;
+            case "d":
+                Debug.Log("Moving right");
+                myRigidbody.velocity = Vector2.right * Speed;
+                break;
         }
     }
 }
