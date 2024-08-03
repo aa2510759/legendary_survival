c8284807fcd3d10e8d778e99c055b6fc0feba0ba
N-a-n-0 <84410640+N-a-n-0@users.noreply.github.com>
2024-08-02 17:54:49 -0700

    Added a random Merchant spawn
    Added a way to make a random Merchant spawn, but the merchant does not do anything yet we only have his random spawning.

--- Assets/Scripts/Map/RandomSpawn.cs
+++ Assets/Scripts/Map/RandomSpawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class RandomSpawn : MonoBehaviour
 {
-    public GameObject obj;
+    public GameObject[] obj;
 
 
     public int x;
@@ -14,15 +14,36 @@ public class RandomSpawn : MonoBehaviour
     public Vector2 vector2;
 
     public int randomSpawnAmount;
+
+    public bool spawnCustom = true;
+
+    public int customX;
+    public int customY;
+
     // Start is called before the first frame update
     void Start()
     {
         randomSpawnAmount = UnityEngine.Random.Range(0, 10);
 
+
+        //Merchant spawn IF STATEMENT RIGHT HERE ************
+        if(spawnCustom == true && (randomSpawnAmount % 2) == 0)
+        {
+            spawnCustom = false;
+            GameObject clone = Instantiate(obj[1]);
+
+            customX = 3;
+            customY = 2;
+
+            vector2 = new Vector2(customX, customY);
+
+            clone.transform.position = vector2;
+        }
+
         for (int i = 0; i < randomSpawnAmount; i++)
         {
             //print("Tree should spawn here");
-            GameObject clone = Instantiate(obj);
+            GameObject clone = Instantiate(obj[0]);
             x = UnityEngine.Random.Range(-5, 5);
             y = UnityEngine.Random.Range(-5, 5);
             //print("Vector2: " + "X: " + x.ToString() +"Y: " + y.ToString());
@@ -31,5 +52,6 @@ public class RandomSpawn : MonoBehaviour
             clone.transform.position = vector2;
 
         }
+
     }
 }
